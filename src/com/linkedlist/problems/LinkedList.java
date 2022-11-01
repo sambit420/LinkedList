74c4478234892bb41e866c2677a48f977b039bb5
sambit420 <sambit2024@gmail.com>
2022-11-02 02:08:04 +0530

    Added : Use Case 4 Program - Add a given node having value 30 in between 56 and 70 in linked list

--- src/com/linkedlist/problems/LinkedList.java
+++ src/com/linkedlist/problems/LinkedList.java
@@ -2,47 +2,57 @@
 //Use Case 1 is to create a simple linked list of 56,30 and 70.
 //Use Case 2 is to create LinkedList by adding 30,56 to 70. Sequence is 56-->30-->70 but adding vice-versa.
 //Use Case 3 is to create a LinkedList by appending 30 and 70 to 56. Sequence 56-->30-->70.
-
+//Use case 4 is to add 30 in between 56 and 70 in the linked list. Sequence is 56-->30-->70.
 package com.linkedlist.problems;
 
 public class LinkedList {
 
-    static class Node
-    {
+    static class Node {
         int data;
         Node next;
-        Node(int data)
-        {
+
+        Node(int data) {
             this.data = data;
             this.next = null;
         }
     }
+
     Node head = null;
     Node tail = null;
 
     //Adding to linked list.
-    public void addToLinkedList(int data)
-    {
+    public void addToLinkedList(int data) {
         Node newNode = new Node(data);
-        if (head == null)
-        {
+        if (head == null) {
             head = newNode;
-        }
-        else
-        {
+        } else {
             tail.next = newNode;
             newNode.next = null;
         }
         tail = newNode;
     }
 
-    //Print LinkedList
-    public void printLinkedList()
-    {
+    //Add the node in the middle.
+    public void addAfterParticularNode(int value) {
         Node current = head;
-        while (current != null)
-        {
-            System.out.print(current.data+"  ");
+        while (current != null) {
+            if (!(current.data == 56)) {
+                current = current.next;
+            } else if (current.data == 56) {
+                break;
+            }
+        }
+        Node newNode = new Node(value);
+        Node temp = current.next;
+        current.next = newNode;
+        newNode.next = temp;
+    }
+
+    //Print LinkedList
+    public void printLinkedList() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + "  ");
             current = current.next;
         }
     }
@@ -51,10 +61,11 @@ public class LinkedList {
         LinkedList list = new LinkedList();
         System.out.println("Welcome to LinkedList program");
         list.addToLinkedList(56);//56 is added first
-        list.addToLinkedList(30);//30 is added after 56
-        list.addToLinkedList(70);//70 is added after 30
+        list.addToLinkedList(70);
+        list.printLinkedList();
+        list.addAfterParticularNode(30);
+        System.out.println();
         list.printLinkedList();
     }
 
-    }
-
+}
