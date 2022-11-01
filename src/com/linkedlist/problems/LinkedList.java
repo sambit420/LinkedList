17c18b9d103764417d4a289e755bde82f3088258
sambit420 <sambit2024@gmail.com>
2022-11-02 00:31:32 +0530

    Added : Display Welcome to Generic Assignment Problem

---
+++ src/com/linkedlist/problems/LinkedList.java
@@ -0,0 +1,8 @@
+package com.linkedlist.problems;
+
+public class LinkedList {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to linked list Problems");
+    }
+}
