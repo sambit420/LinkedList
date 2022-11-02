56d58595aa81eb42ddf0b3383a4563aed5081678
sambit420 <sambit2024@gmail.com>
2022-11-02 19:47:08 +0530

    Added : Use Case 8 Program - Adding a node at a given position in the

--- src/com/linkedlist/problems/LinkedList.java
+++ src/com/linkedlist/problems/LinkedList.java
@@ -1,41 +1,47 @@
 //This class is all about LinkedList Problem.
 //Use Case 1 is to create a simple linked list of 56,30 and 70.
 //Use Case 2 is to create LinkedList by adding 30,56 to 70. Sequence is 56-->30-->70 but adding vice-versa.
-//Use Case 3 is to create a LinkedList by appending 30 and 70 to 56. Sequence 56-->30-->70.
-//Use case 4 is to add 30 in between 56 and 70 in the linked list. Sequence is 56-->30-->70.
-//Use case 5 is to delete first element in the linked list. Given sequence is 56-->30-->70 and o/p will be 30-->70.
+//Use Case 3 is to create a LinkedList by appending 30 and 70 to 56. Sequence is 56-->30-->70.
+//Use Case 4 is to add 30 in between 56 and 70 in the linked list. Sequence is 56-->30-->70.
+//Use Case 5 is to delete first element in the linked list. Given sequence is 56-->30-->70 and o/p will be 30-->70.
 //Use Case 6 is to delete last element in the linked list. Given sequence is 56-->30-->70 and o/p will be 56-->30.
 //Use Case 7 is to search linked list to find node with value 30. Given sequence is 56-->30-->70.
-
+//Use Case 8 is to insert 40 after 30 to the linked list. Given sequence is 56-->30-->70 and o/p will be 56-->30-->40-->70.
 
 package com.linkedlist.problems;
 
 public class LinkedList {
 
-    static class Node {
+    static class Node
+    {
         int data;
         Node next;
-
-        Node(int data) {
+        Node(int data)
+        {
             this.data = data;
             this.next = null;
         }
     }
-
     Node head = null;
     Node tail = null;
+    static int position;
 
     //Adding to linked list.
-    public void addToLinkedList(int data) {
+    public void addToLinkedList(int data)
+    {
         Node newNode = new Node(data);
-        if (head == null) {
+        if (head == null)
+        {
             head = newNode;
-        } else {
+        }
+        else
+        {
             tail.next = newNode;
             newNode.next = null;
         }
         tail = newNode;
     }
+
     //Search the LinkedList to find a particular node
     public void checkNode(int value)
     {
@@ -46,6 +52,7 @@ public class LinkedList {
             if (current.data == value)
             {
                 System.out.println("Searched key : "+value+" is present at node : "+count);
+                position = count;
                 return;
             }
             current = current.next;
@@ -53,8 +60,27 @@ public class LinkedList {
         }
         System.out.println("Searched key is not present in the LinkedList");
     }
+
+    //Add the node in the middle.
+    public void addAtParticularPosition(int value, int index)
+    {
+        Node current = head;
+        int count = 1;
+        while (current != null && count != index)//List starts from 0 so 30 position is 1 and this index
+        //represents 2 where 40 will be put.
+        {
+            current = current.next;
+            count++;
+        }
+        Node newNode = new Node(value);
+        Node temp = current.next;
+        current.next = newNode;
+        newNode.next = temp;
+    }
+
     //Print LinkedList
-    public void printLinkedList() {
+    public void printLinkedList()
+    {
         Node current = head;
         if (head == null)
         {
@@ -71,12 +97,16 @@ public class LinkedList {
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
         System.out.println("Welcome to LinkedList program");
-        list.addToLinkedList(56);//56 is added first
+        list.addToLinkedList(56);
         list.addToLinkedList(30);
         list.addToLinkedList(70);
+        System.out.println("Before adding 40 to the LinkedList");
         list.printLinkedList();
         System.out.println();
-     list.checkNode(30);
-    }
+        list.checkNode(30);
+        list.addAtParticularPosition(40,position);//position represents the node position of 30.
+        System.out.println("After adding 40 to the LinkedList");
+        list.printLinkedList();
 
+    }
 }
